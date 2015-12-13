768993c5abed143b196df9d69b736e0ce4676a32
Wei Wei <lyweiwei@outlook.com>
2015-12-13 15:57:39 +0800

    Get the general functionality work.

--- test/index.js
+++ test/index.js
@@ -8,4 +8,34 @@ describe('MingTemplate', function () {
     expect(new MingTemplate()).to.be.instanceof(MingTemplate);
   });
 
+  const template = `
+    //
+    // @ming NAME some body's name
+    // @ming STATEMENT some statements
+    //
+    function hello_$NAME$() {
+      console.log('Hello, $NAME$');
+      $STATEMENT$;
+    }
+  `;
+
+  it('should collect the MINGs', () => {
+    const tmpl = new MingTemplate(template);
+    expect(tmpl.mings).to.be.deep.equal({
+      NAME: 'some body\'s name',
+      STATEMENT: 'some statements'
+    });
+  });
+
+  it('should render the template', () => {
+    const tmpl = new MingTemplate(template);
+    const result = tmpl.render({
+      NAME: 'World',
+      STATEMENT: 'console.log("lalala...")'
+    });
+    const exp = `function hello_World() {\n    console.log('Hello, World');\n    console.log('lalala...');\n}`;
+    expect(result).to.equal(exp);
+
+  });
+
 });
